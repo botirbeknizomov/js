9492c2f2ff328a9c9a74c139771cff71bc697538
botirbeknizomov <botirbeknizomov1@gmail.com>
2023-11-07 13:58:47 +0500

    vazifa

---
+++ main.js
@@ -0,0 +1,65 @@
+//Varitables-o`zgaruvchilar//
+//string, number, boolean, null, underfined, object //
+
+//var
+//let
+//const
+
+//var myName = "Botirbek";
+//var myYear = 2008;
+//var myAge = 15;
+
+//myAge = 16;
+//myAge = 18;
+//myAge = myAge + 16;
+
+//var myAge = myAge + myAge
+
+//console.log(myName, myAge, myYear);
+
+//let myCity;
+//myCity = 'Bukhara'
+//console.log(myCity)
+
+//const
+
+//===================================//
+// Stringni stringa qo`shish //
+//let text = "Hello"
+
+//console.log(text)
+
+// let myName = 'Botirbek '
+// let mySurname = "Nizomov"
+// let myAge = 15
+// console.log( myName + mySurname + " " + myAge)
+
+// console.log(`Hello my name is ${myName} ${mySurname}. Hello i am years old ${myAge}`)
+// console.log(`Hello i am years old ${myAge}`)
+// console.log()
+
+//===================================//
+
+//Amallar; matematik amallar, taqqoslash amallari, mantiqiy amallar//
+
+// *  ko`paytirish
+// -  ayirish
+// +  qo`shish
+// /  bo`lish
+// %  qoldiq
+//**  daraja
+
+//let x = 10
+
+//x  = x ** 9;
+//console.log(`x: ${x}`)
+
+let Age = +prompt("Necha yoshdasiz?");
+console.log(Age);
+
+let monthly = prompt(`Oylik daromadingiz qancha? `);
+
+let oylik = 1200000;
+oylik = oylik / 100;
+
+console.log(` Salom,siz ${oylik} ming jarima to'lashingiz kerak.`);
